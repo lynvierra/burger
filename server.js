003056a6cf949c3ec7cb28af093f120e96b43f6e
Lyn Vierra <lynvierra@gmail.com>
2018-08-26 00:13:35 -0700

    final push

--- server.js
+++ server.js
@@ -1,56 +1,25 @@
-// ************************************************************
-// Program: Eat-Da-Burger App
-// Author: Rod Skoglund
-// File: server.js
-// ************************************************************
-
-// ************************************************************
-// Dependencies
-// ************************************************************
 var express = require("express");
 var bodyParser = require("body-parser");
 
-// ************************************************************
-// Define PORT to use - local or on Heroku
-// ************************************************************
 var PORT = process.env.PORT || 3000;
 
 var app = express();
 
-// ************************************************************
-// Serve static content for the app from the "public" directory 
-// in the application directory.
-// ************************************************************
 app.use(express.static("public"));
 
-// ************************************************************
-// parse application/x-www-form-urlencoded
-// ************************************************************
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// ************************************************************
-// parse application/json
-// ************************************************************
 app.use(bodyParser.json());
 
-// ************************************************************
-// Set Handlebars.
-// ************************************************************
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// ************************************************************
-// Import routes and give the server access to them.
-// ************************************************************
 var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
-// ************************************************************
-// Start listener
-// ************************************************************
 app.listen(PORT, function() {
   console.log("App now listening at localhost:" + PORT);
 });
